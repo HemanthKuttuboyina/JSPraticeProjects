64bad8d8b2dfff443de9ab898f1e634424ba6cb5
Hemanth K <khemu4567@gmail.com>
2025-02-28 19:49:55 +0000

    New Commit

--- clock.js
+++
@@ -1,7 +0,0 @@
-
-const clk = document.querySelector("#time")
-
-setInterval(() => {
-    let date = new Date
-    clk.innerHTML = date.toLocaleTimeString();
-}, 1000);
--- colorChange.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll(".button");
-const body = document.querySelector("body")
-buttons.forEach((btn)=>{
-    btn.addEventListener("click",(e)=>{
-        body.style.backgroundColor = e.target.id;
-    })
-})
